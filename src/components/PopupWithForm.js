368542366bc668fb497f70ee19cf54694b2111d6
vyacheslav <clavatar@yandex.ru>
2022-02-22 19:16:21 +0300

    some bugfix

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,7 +3,9 @@
 function PopupWithForm(props) { 
     return (
         //редактирование профиля
-        <div className={`popup popup_type_${props.name} ${props.isOpen && 'popup_opened'}`} onClick={() => props.onClose()}>
+        <div className={`popup popup_type_${props.name} ${props.isOpen && 'popup_opened'}`} onClick={(evt) => {
+            if (evt.target===evt.currentTarget) {props.onClose()}
+        }}>
             <div className="popup__container">
                 <form className="popup__input-form popup__form" name={`${props.name}-form`} novalidate>
                     {props.children}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode className="page">
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
